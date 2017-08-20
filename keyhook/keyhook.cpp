9dac74c9919e19afb5d218c597b004ba2349e037
Barbacamanitu <russmbiz@gmail.com>
2017-08-20 15:28:11 -0500

    Setup exit handlers

--- keyhook/keyhook.cpp
+++ keyhook/keyhook.cpp
@@ -5,37 +5,68 @@
 #include "KeyboardHook.h"
 #include <iostream>
 //#define _WIN32_WINNT 0x050
+bool isRegistered = false;
+void unregister();
 
+BOOL WINAPI ConsoleHandlerRoutine(DWORD dwCtrlType) {
+	if (CTRL_CLOSE_EVENT == dwCtrlType) {
+		unregister();
+		return TRUE;
+	}
 
+	return FALSE;
+}
 
-int main()
-{
+int messageLoop() {
 	MSG msg;
 	BOOL bRet;
-	KeyboardHook::getInstance().registerHook();
-
-	
-
 	while ((bRet = GetMessage(&msg, NULL, 0, 0)) != 0)
 	{
-			if (bRet == -1)
-			{
-				// handle the error and possibly exit
-				int a = 1;
-			}
-			else
-			{
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
-			}
+		if (bRet == -1)
+		{
+			// handle the error and possibly exit
+			return bRet;
+		}
+		else
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
 	}
-	KeyboardHook::getInstance().unregisterHook();
-
-
-	std::cout << "Done" << std::endl;
-	system("pause");
-	// Return the exit code to the system. 
 
 	return msg.wParam;
+}
+
+void setupExitHandler() {
+	std::cout << "Setting up exit handler..." << std::endl;
+	atexit(unregister);
+	if (FALSE == SetConsoleCtrlHandler(ConsoleHandlerRoutine, TRUE)) {
+		// Cannot register your handler? Check GetLastError()
+		std::cout << "Couldn't set up exit handler." << std::endl;
+	}
+	else {
+		std::cout << "Sucessfully set up exit handler!" << std::endl;
+	}
+}
+
+void unregister() {
+	if (isRegistered) {
+		std::cout << "Unregistering Keyboard Hook..." << std::endl;
+		KeyboardHook::getInstance().unregisterHook();
+		std::cout << "Keyboard Hook Unregistered!" << std::endl;
+	}
+}
+int main()
+{
+	KeyboardHook::getInstance().registerHook();
+	isRegistered = true;
+
+	setupExitHandler();
+	std::cout << "Hook Registered." << std::endl;
+	
+	std::cout << "Starting message loop..." << std::endl;
+	int result = messageLoop();
+	std::cout << "Message loop ended. Terminating program." << std::endl;
+	return result;
 	//return(0);
 }
